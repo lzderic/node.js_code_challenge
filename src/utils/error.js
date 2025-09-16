dd5a634e47d2e4d2ce1a681d5e1e44c5a42a449c
lzderic <76105867+lzderic@users.noreply.github.com>
2025-09-16 14:18:46 +0200

    feat: add JSDoc comments for error utility methods

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,8 +1,17 @@
+/**
+ * Prints an error message to stderr and exits the process with the given exit code.
+ * @param {string} message - The error message to print.
+ * @param {number} [code=1] - The exit code to use (default is 1).
+ */
 export function printErrorAndExit(message, code = 1) {
   process.stderr.write(message + '\n');
   process.exit(code);
 }
 
+/**
+ * Prints an error message to stderr without exiting the process.
+ * @param {string} message - The error message to print.
+ */
 export function printError(message) {
   process.stderr.write(message + '\n');
 }
